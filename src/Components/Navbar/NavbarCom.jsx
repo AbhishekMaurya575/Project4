4027b74d135213a913e63c0c3693f4e90692de8b
AbhishekMaurya575 <abhishek.maurya257185@gmail.com>
2024-12-13 19:45:00 +0530

    Update NavbarCom.jsx

--- src/Components/Navbar/NavbarCom.jsx
+++ src/Components/Navbar/NavbarCom.jsx
@@ -1,12 +1,27 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const NavbarCom = ({className}) => {
   return (
     <div className={`bg-none w-[25%] md:w-[35%] lg:w-[25%] justify-around text-xl items-center flex gap-4 ${className}`}>
-      <button className="">Home</button>
-      <button className="">About</button>
-      <button className="">Shop</button>
-      <button className="">Contact</button>
+     <NavLink
+  to=""
+  className={({ isActive }) =>
+    isActive ? " font-bold" : ""
+  }
+>
+  <button>Home</button>
+</NavLink>
+
+     <NavLink to="About"     className={({ isActive }) =>
+    isActive ? " font-bold" : ""
+          }> <button className="">About</button></NavLink>
+      <NavLink to="Shop"     className={({ isActive }) =>
+    isActive ? " font-bold" : ""
+          }>  <button className="">Shop</button></NavLink>
+     <NavLink to="Contact"    className={({ isActive }) =>
+    isActive ? "font-bold" : ""
+          }>   <button className="">Contact</button></NavLink>
     </div>
   );
 };
