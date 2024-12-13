42ee469b6ca5a7bbd6af80ff25e9a64a18c54c29
AbhishekMaurya575 <abhishek.maurya257185@gmail.com>
2024-12-13 19:45:18 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider, Routes } from 'react-router-dom'
+import Home from './Components/Home/Home.jsx'
 import App from './App.jsx'
+import About from './Components/About/About.jsx'
+import Shop from './Components/Shpo/Shop.jsx'
+import Contact from './Components/Contact/Contact.jsx'
+
+
+const Router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App/>}>
+      <Route path='' element={<Home/>}/>
+      <Route path="About" element={<About/>}/>
+      <Route path="Shop" element={<Shop/>}/>
+      <Route path="Contact" element={<Contact/>}/>
+    </Route>
+  )
+)
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+  <RouterProvider router={Router}/>
   </StrictMode>,
 )
