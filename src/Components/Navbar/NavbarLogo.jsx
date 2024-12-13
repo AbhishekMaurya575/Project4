032004464b688084bf3c48f81361ee38895f140c
AbhishekMaurya575 <abhishek.maurya257185@gmail.com>
2024-12-13 19:45:04 +0530

    Update NavbarLogo.jsx

--- src/Components/Navbar/NavbarLogo.jsx
+++ src/Components/Navbar/NavbarLogo.jsx
@@ -2,7 +2,10 @@ import React from "react";
 
 const NavbarLogo = () => {
   return (
-    <div className=" w-[80%]  md:w-[20%] lg:w-[25%] justify-center items-center flex">name logo</div>
+    <div className=" w-[80%]  md:w-[20%] lg:w-[25%] justify-center items-center flex">
+      
+      <img src="src\assets\Screenshot 2024-12-13 192731.png"  className="h-full w-full"/>
+    </div>
   );
 };
 
