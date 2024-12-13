ac411434102c6461f4e502fd4323e46776b83028
AbhishekMaurya575 <abhishek.maurya257185@gmail.com>
2024-12-13 20:53:16 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,12 @@ import Navbar from "./Components/Navbar/Navbar"
 
 function App() {
 
-
+  const apiUrl = import.meta.env.VITE_API_URL;
+  const apiKey = import.meta.env.VITE_API_KEY;
+  
+  console.log('API URL:', apiUrl);
+  console.log('API Key:', apiKey);
+  
   return (
 
 <div className="h-screen">
